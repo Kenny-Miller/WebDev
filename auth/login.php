e2b8b51880505b18058ce3c799d121a0c23461be
Kenny-Miller <kennymilarhol@gmail.com>
2020-02-17 13:29:19 -0700

    bump

--- auth/login.php
+++ auth/login.php
@@ -1,4 +1,1 @@
-<?php require_once '/shared/head.php'; ?>
-<?php require_once '/shared/header.php'; ?>
-    <div> Welcome to Login</div>
-<?php require_once '/shared/footer.php'; ?>
+<div> Welcome to Login</div>
