126bf8da55dd2dc21551986892985e4033e70a32
Kenny-Miller <kennymilarhol@gmail.com>
2020-02-17 16:02:36 -0700

    updated login css

--- auth/login.php
+++ auth/login.php
@@ -1,12 +1,14 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/head.php'); ?>
-<body class="login">
-    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/header.php'); ?>
-    <div class="login">
-        <form method="POST" action="/users/dashboard.php">
-            <div><input type="text" id="username" value="username"></div>
-            <div><input type="password" id="password" value="password"></div>
-            <div><input type="submit" value="Login"></div>
-        </form>
-    </div>
-    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/footer.php'); ?>
-</body>
+<html>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/head.php'); ?>
+    <body class="login">
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/header.php'); ?>
+        <div class="login">
+            <form class="login" method="POST" action="/users/dashboard.php">
+                <div><input type="text" id="username" value="username"></div>
+                <div><input type="password" id="password" value="password"></div>
+                <div><input type="submit" value="Login"></div>
+            </form>
+        </div>
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/footer.php'); ?>
+    </body>
+</html>
