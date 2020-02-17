c03f07d80a16c9e9c7b03600a236014ad34af454
Kenny-Miller <kennymilarhol@gmail.com>
2020-02-17 15:45:14 -0700

    Added css for login page

--- auth/login.php
+++ auth/login.php
@@ -1,8 +1,12 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/head.php'); ?>
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/header.php'); ?>
-    <form method="POST" action="/users/dashboard.php">
-        <div><input type="text" id="username" value="username"></div>
-        <div><input type="password" id="password" value="password"></div>
-        <div><input type="submit" value="Login"></div>
-    </form>
+<body class="login">
+    <div class="login">
+        <form method="POST" action="/users/dashboard.php">
+            <div><input type="text" id="username" value="username"></div>
+            <div><input type="password" id="password" value="password"></div>
+            <div><input type="submit" value="Login"></div>
+        </form>
+    </div>
+</body>
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/footer.php'); ?>
--- shared/head.php
+++ shared/head.php
@@ -2,4 +2,5 @@
 <head>
     <title>Incident Manager</title>
     <link rel="icon" href="/styles/images/favicon.ico" type="image/x=icon"/>
+    <link rel="stylesheet" href="/styles/style.css" type="text/css"/>
 </head>
