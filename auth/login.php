283322eb8f84238601edbf4521c4d1148cc034d8
Kenny-Miller <kennymilarhol@gmail.com>
2020-02-17 16:13:21 -0700

    updated login css

--- auth/login.php
+++ auth/login.php
@@ -4,8 +4,8 @@
         <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/header.php'); ?>
         <div class="login">
             <form class="login" method="POST" action="/users/dashboard.php">
-                <div><input type="text" id="username" value="username"></div>
-                <div><input type="password" id="password" value="password"></div>
+                <div><input type="text" id="username" name="username" value="username"></div>
+                <div><input type="password" id="password" name="password" value="password"></div>
                 <div><input type="submit" value="Login"></div>
             </form>
         </div>
