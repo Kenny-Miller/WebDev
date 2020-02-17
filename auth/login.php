81203823d19836273ca806d8060b468561fd5f12
Kenny-Miller <kennymilarhol@gmail.com>
2020-02-17 13:41:43 -0700

    test php changes

--- auth/login.php
+++ auth/login.php
@@ -1,2 +1,4 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/head.php'); ?>
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/header.php'); ?>
 <div> Welcome to Login</div>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/footer.php'); ?>
