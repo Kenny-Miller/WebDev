72675bc1717ca5745b8f6ed5ea297d609f47e4a3
Kenny-Miller <kennymilarhol@gmail.com>
2020-02-18 19:18:56 -0700

    added other pages

--- auth/login.php
+++ auth/login.php
@@ -1,13 +1,19 @@
 <html>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/head.php'); ?>
-    <body class="login">
+    <body>
         <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/header.php'); ?>
-        <div class="login">
-            <form class="login" method="POST" action="/users/dashboard.php">
-                <div class="form-heading"><h3 class="formTitle">Login</h3></div>
-                <div class="loginRow"><input class="form-input" type="text" class="login" name="username" value="username"></div>
-                <div class="loginRow"><input class="form-input" type="password" class="login" name="password" value="password"></div>
-                <div class="loginRow"><input class="submitbtn form-input"  type="submit" value="Login"></div>
+        <div class="login-tile">
+            <form class="login-form" method="POST" action="/users/dashboard.php">
+                <div class="login-form-heading"><h1>Login</h1></div>
+                <div class="login-form-row">
+                    <h3>Username</h3>
+                    <input class="login-form-input" type="text"  name="username" value="username">
+                </div>
+                <div class="login-form-row">
+                    <h3>Password</h3>
+                    <input class="login-form-input" type="password"  name="password" value="password">
+                </div>
+                <div class="login-form-row"><input class="login-submitbtn login-form-input"  type="submit" value="Login"></div>
             </form>
         </div>
         <?php require_once($_SERVER['DOCUMENT_ROOT'].'/shared/footer.php'); ?>
