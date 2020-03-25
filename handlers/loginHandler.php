d2cbbe40023afa71d338849c7081dc07088dff99
Kenny-Miller <kennymilarhol@gmail.com>
2020-03-25 08:38:55 -0600

    bump

--- handlers/loginHandler.php
+++ handlers/loginHandler.php
@@ -7,7 +7,7 @@
     
     $dao = new Dao();
     
-    echo $dao->userExists($_POST['username'], $_POST['password']);
+    $result = $dao->userExists($_POST['username'], $_POST['password']);
     
     if ($dao->userExists($_POST['username'], $_POST['password'])) {
         $_SESSION['auth'] = true;
@@ -16,7 +16,6 @@
     } else {
         $_SESSION['auth'] = false;
         $_SESSION['message'] = "Invalid username or password";
-        header("Location: https://frozen-ravine-42740.herokuapp.com/pages/login.php");
-        exit;
+        echo "you go {$result}";
     }
 ?>
