361404630138a3d78b5baac69bb1fb8fca5565d1
Kenny-Miller <kennymilarhol@gmail.com>
2020-03-24 22:35:25 -0600

    bump

--- handlers/loginHandler.php
+++ handlers/loginHandler.php
@@ -2,14 +2,13 @@
     session_start();
     
     
-
     if ($username == $_POST['username'] && $password == $_POST['password']) {
         $_SESSION['auth'] = true;
-        header("Location: http:/pages/workspace.html");
+        header("Location: https://frozen-ravine-42740.herokuapp.com/pages/workspace.html");
         exit;
     } else {
         $_SESSION['auth'] = false;
         $_SESSION['message'] = "Invalid username or password";
-        header("Location: http:/pages/login.php");
+        header("Location: https://frozen-ravine-42740.herokuapp.com/pages/pages/login.php");
     }
 ?>
