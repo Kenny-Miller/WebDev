aa54dfe4877758958cc1ef40b8258140382ce938
Kenny-Miller <kennymilarhol@gmail.com>
2020-03-25 17:10:32 -0600

    bump

--- pages/workspaces.php
+++ pages/workspaces.php
@@ -27,8 +27,7 @@
             <?php 
                     if (isset($_SESSION["message"])) {
                         echo print_r($_SESSION);
-                        unset($_SESSION["messsage"]);
-                        $_SESSION["message"] = null;
+                        unset($_SESSION["message"]);
                         echo print_r($_SESSION);
                     }
                 ?>
