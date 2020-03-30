2073dbea7b44ae3b91dd6caa9a005620d1fa4c97
Kenny-Miller <kennymilarhol@gmail.com>
2020-03-29 21:02:49 -0600

    bump

---
+++ pages/addTask.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>Incident Manager</title>
+        <link rel="icon" href="/resources/images/favicon.ico" type="image/x=icon"/>
+        <link rel="stylesheet" href="/resources/style.css" type="text/css"/>
+    </head>
+    <body>
+        <div class="header">
+            <img src="/resources/images/logo.png" alt="Logo">
+        </div>
+        <div class="container">
+            <form class="task-form" method="POST" action="/pages/addTaskHandler.php">
+                <h1 class="heading">Add Task</h1>
+                <div class="task-form-row">
+                    <select>
+                    
+                    
+                    </select>
+                </div>
+                <div class="task-form-row">
+                    <input type="textarea">
+                </div>
+                <div class="task-form-row">
+                    <select>
+                    
+                    </select>
+                </div>
+                <div class="task-form-row">
+                    <input id="task-submit"  type="submit" value="AddTask">
+                </div>
+            </form>
+        </div>
+        <div class="footer">@Copyright 2020 Kenny Miller</div>
+    </body>
+</html>
