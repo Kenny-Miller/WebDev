186471b44a0aa79baa9a8d5cfd4293642cd1808c
Kenny-Miller <kennymilarhol@gmail.com>
2020-03-30 17:23:40 -0600

    updated add task css

--- handlers/addTaskHandler.php
+++ handlers/addTaskHandler.php
@@ -52,8 +52,10 @@
         header("Location: https://frozen-ravine-42740.herokuapp.com/pages/dashboard.php?wid={$wid}");
         exit;
     } else{
-        $dao->addTask($wid,$sid,$text, $uid['user_id']);
-        header("Location: https://frozen-ravine-42740.herokuapp.com/pages/workspaces.php?wid={$wid}");
-        exit;
+        echo print_r($uid);
+       // $dao->addTask($wid,$sid,$text, $uid['user']);
+       // header("Location: https://frozen-ravine-42740.herokuapp.com/pages/workspaces.php?wid={$wid}");
+        
+       // exit;
     }  
 ?>
